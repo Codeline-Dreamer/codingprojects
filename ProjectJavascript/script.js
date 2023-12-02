db3db228dd4635c21fc042163b5115f5f3d80f18
Codeline-Dreamer <jatin@virtualcodetutor.in>
2023-12-02 19:26:39 +0530

    Tested Variables

--- ProjectJavascript/script.js
+++ ProjectJavascript/script.js
@@ -1,4 +1,29 @@
 //This is a comment
-
+/*
 let myName = "Jatin";
 console.log(myName);
+*/
+
+const age ="45";
+const myName ="Jatinder";
+console.log ("My age is " + age + " My name is " + myName);
+
+function testScope (){
+let carName = "Jeep";
+const carModel = "Dynamic Model";
+
+console.log (carName);
+console.log (carModel);
+}
+
+testScope();
+
+var mySport = "Cricket";
+console.log(mySport);
+
+let myPet = "Cat";
+console.log(myPet);
+
+const mySalary = "1 Billion";
+console.log (mySalary);
+
