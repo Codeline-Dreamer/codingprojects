a5d674e0c8eb3cf58cc073b1f91a7d682c401662
Codeline-Dreamer <jatin@virtualcodetutor.in>
2023-12-03 13:23:54 +0530

    Added Description

--- ProjectJavascript/script.js
+++ ProjectJavascript/script.js
@@ -2,7 +2,6 @@
 /*
 let myName = "Jatin";
 console.log(myName);
-*/
 
 const age ="45";
 const myName ="Jatinder";
@@ -26,4 +25,29 @@ console.log(myPet);
 
 const mySalary = "1 Billion";
 console.log (mySalary);
+*/
 
+//Math Operators
+const now = 2023
+const ageJatin = now -1979;
+const ageViraj = now - 2011;
+console.log(ageJatin, ageViraj);
+
+const firstName = "Jatin";
+const lastName = "Singh";
+console.log(firstName + " " + lastName);
+
+//assignment Operator
+let x = 10 + 5; //15
+x += 10; // x = x + 15 , which is x = 15 + 10
+x *= 4; // x = x * 4, which is 25 * 4 = 100
+x++; // x = x + 1
+x--;
+console.log(x);
+
+console.log(ageJatin > ageViraj);
+console.log(ageViraj > ageJatin);
+
+const ageCriteria = 18;
+console.log("age criteria check " + (ageJatin >= ageCriteria));
+console.log("age criteria check "  + (ageViraj >= ageCriteria)); 
